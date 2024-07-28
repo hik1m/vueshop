215246e29ccdf155a8a3ded34c3c9e170f5ca7df
hik1m <fel1x.kanbekov@yandex.ru>
2024-07-28 18:24:28 +0500

    первый коммит

---
+++ src/components/CartItemList.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { inject } from 'vue'
+import CartItem from './CartItem.vue'
+const { cart, removeFromCart } = inject('cart')
+</script>
+<template>
+  <div class="grid flex-col flex-1 gap-4 justify-betweeen" v-auto-animate>
+    <CartItem
+      v-for="item in cart"
+      v-bind:key="item.id"
+      :title="item.title"
+      :price="item.price"
+      :image-url="item.imageUrl"
+      @on-click-remove="() => removeFromCart(item)"
+    />
+  </div>
+</template>
